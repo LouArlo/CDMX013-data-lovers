246efe5be1db05f66e129fe2381414047b83b80c
LouArlo <lulu.arlo1306@gmail.com>
2022-08-08 10:05:58 -0500

    Avance historia 1

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
-import { example } from './data.js';
+import data from "./data/harrypotter/data.js";
+/*import { example } from './data.js';*/
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+console.log(data, data);
+const showBiography = document.getElementById("name");
+
+showBiography.addEventListener('click', function() {
+    const name = document.getElementById('name').ariaValueText;
+
+    document.getElementById("name").innerHTML = (data.showBiography(name));
+})
