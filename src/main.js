d411bc85d8f39d7eb5b90efa980ad164b2751b4d
MayanSantiago17 <mayansantiago1703@gmail.com>
2022-08-09 17:31:02 -0500

    despliegue de data en scroll

--- src/main.js
+++ src/main.js
@@ -1,13 +1,50 @@
-import data from "./data/harrypotter/data.js";
-/*import { example } from './data.js';*/
-// import data from './data/lol/lol.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import alldata from './data/harrypotter/data.js'
 
-console.log(data, data);
-const showBiography = document.getElementById("name");
+const root = document.getElementById('root')
 
-showBiography.addEventListener('click', function() {
-    const name = document.getElementById('name').ariaValueText;
+root.classList='HpData-style'
 
-    document.getElementById("name").innerHTML = (data.showBiography(name));
-})
+const character=alldata.characters
+
+
+
+const generatorHTML=(characters)=>{
+    const div = document.createElement('div')
+
+    const nameCharacter = document.createElement('h2')
+    nameCharacter.textContent=characters.name
+
+    const img = document.createElement('img')
+    img.setAttribute('src',characters.img)
+
+   /*if (img === undefined) {
+        img.src = '/images/HP_personajes_extra.png'
+        console.log(img)
+    }*/
+
+    div.append(img,nameCharacter)
+    return div 
+
+}
+
+character.forEach(onecharacters=>root.appendChild(generatorHTML(onecharacters))
+
+
+)
+
+   /* let characterButton = div.cloneNode(true);
+    characterButton.children[1].src = characters.img;
+    characterButton.children[0].innerHTML = characters.name;
+    characterButton.id = characters.id;
+
+    characterButton.addEventListener('click',function(){dataWindow(characterButton.id); });
+    document.getElementById('info_characters').appendChild(characterButton);
+    characterButton.style.display = 'block';
+   */
+   
+
+/* 
+const x = document.addEventListener(click)
+characters
+
+*/
