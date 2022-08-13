d8088df41655f9143c119b51cd7dfbef116b5131
MayanSantiago17 <mayansantiago1703@gmail.com>
2022-08-12 19:11:30 -0500

    navBar con clicks

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
+    window.onscroll = function() {myFunction()};
+      
+      var navbar = document.getElementById("navigation");
+      var sticky = navbar.offsetTop;
+      
+      function myFunction() {
+        if (window.pageYOffset >= sticky) {
+          navbar.classList.add("sticky")
+        } else {
+          navbar.classList.remove("sticky");
+        }
+      }
+
 import alldata from './data/harrypotter/data.js'
 
 const root = document.getElementById('root')
@@ -30,15 +43,58 @@ const generatorHTML=(characters)=>{
     return div 
 }
 
-
-
 character.forEach(onecharacters=>root.appendChild(generatorHTML(onecharacters)))
 
 
-
-
-/*info.addEvenListener('click', function() {
-alert('estoy escuchando el click')*/
-
+document.getElementById("characters").addEventListener("click",function(){
+    alert("character")
+}, true);
     
+
+
+
+
+document.getElementById("spells").addEventListener("click",function(){
+    alert("spells")
+
+document.getElementById("root").innerHTML= "";
+
+const root = document.getElementById('root')
+
+root.classList='HpData-style'
+
+/*seleccion de characters*/
+
+const spell=alldata.spells 
+
+const generatorHTML=(spells)=>{
+
+
+    const div = document.createElement('div')
+
+    const nameSpell = document.createElement('h2')
+    nameSpell.textContent=spells.name
+
+    const descriptionSpell = document.createElement('h2')
+    descriptionSpell.textContent=spells.description
+
+    /*const img = document.createElement('img')
+    img.setAttribute('src',`${spells.img}`)
+
+    if (spells.img === undefined) {
+        img.setAttribute('src','./images/HP_personajes_extra.png')
+    }*/
+
+    div.append (nameSpell,descriptionSpell)
+    
+    
+    return div 
+}
+
+
+
+spell.forEach(onespell=>root.appendChild(generatorHTML(onespell)))
+
+},true);
+
 
