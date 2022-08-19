9da18f2ee2cd499f332ba9afbb65978e144e90bd
LouArlo <lulu.arlo1306@gmail.com>
2022-08-18 20:03:26 -0500

    Entregable historia 2

--- src/dataF.js
+++ src/dataF.js
@@ -1,11 +1,12 @@
-export function filterbygrifindor(alldata){
- return alldata.characters.filter(function(chars){
-    return chars.house == "Gryffindor"
+export function filterbyword(alldata, wordfilter) {
+  return alldata.characters.filter(function (chars) {
+    return chars.house == wordfilter
   })
 }
 
-export function filterbyslytherin(alldata){
-  return alldata.characters.filter(function(chars){
-     return chars.house == "Slytherin"
-   })
- }
+export function filterbyword2(alldata, wordfilter) {
+  return alldata.spells.filter(function (chars) {
+    return chars.spell_type == wordfilter
+    console.log()
+  })
+}
--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import alldata from './data/harrypotter/data.js';
-import generatorCharacter from './generatorHTML.js'
-import {filterbygrifindor} from './dataF.js'
-import {filterbyslytherin} from './dataF.js'
+import {generatorHTMLfilter, generatorHTML, generatorHTMLdescribe, generatorHTMLff, generatorHTMLbook} from './generatorHTML.js'
+import {filterbyword, filterbyword2} from './dataF.js'
 
+// ------------navbar-----------------------
 window.onscroll = function () { myFunction() };
 
 var navbar = document.getElementById("navigation");
@@ -17,185 +17,103 @@ function myFunction() {
 }
 
 
-/*----------------Pantalla inicial----------------*/
-
+//---------------Carga de data-historia 1----------------
 const root = document.getElementById('root')
 root.classList = 'HpData-style'
 
 const character = alldata.characters
-const generatorHTML = (characters) => {
-  const div = document.createElement('div')
-  const nameCharacter = document.createElement('h2')
-  nameCharacter.textContent = characters.name
-  const img = document.createElement('img')
-  img.setAttribute('src', `${characters.img}`)
- 
-  if (characters.img === undefined) {
-    img.setAttribute('src', './images/HP_personajes_extra.png')
-  }
-
-  div.append(img, nameCharacter)
-  return div
-} 
 
 character.forEach(onecharacters => root.appendChild(generatorHTML(onecharacters)))
 
-
-
-/*-------characters    grid------------------*/
+//-------filtrado por objeto(characters) ------------------
 document.getElementById("characters").addEventListener("click", function () {
   document.getElementById("root").innerHTML = "";
 
   const root = document.getElementById('root')
-        root.classList = 'HpData-style'
-  
+  root.classList = 'HpData-style'
+
   const character = alldata.characters
 
-  
 
-  character.forEach(onecharacters => root.appendChild(generatorCharacter(onecharacters)))
+  character.forEach(onecharacters => root.appendChild(generatorHTML(onecharacters)))
 
 }, true);
 
 
 
-/*------------------ spells grid----------------*/
+//-------filtrado por objeto(spells) ----------------
 document.getElementById("spells").addEventListener("click", function () {
   document.getElementById("root").innerHTML = "";
 
   const root = document.getElementById('root')
-        root.classList = 'HpDataS-style'
- 
+  root.classList = 'HpDataS-style'
+
   const spell = alldata.spells
 
-  const generatorHTML = (spells) => {
-    const div = document.createElement('div')
-    const nameSpell = document.createElement('h2')
-    nameSpell.textContent = spells.name
-    const descriptionSpell = document.createElement('h3')
-    descriptionSpell.textContent = spells.description
-
-    div.append(nameSpell, descriptionSpell)
-
-    return div
-  }
-
-  spell.forEach(onespell => root.appendChild(generatorHTML(onespell)))
+  spell.forEach(onespell => root.appendChild(generatorHTMLdescribe(onespell)))
 
 }, true);
 
-
-
-/* ------------------fun Fact grid----------------*/
+// ------filtrado por objeto (fun Fact)----------------
 document.getElementById("fun facts").addEventListener("click", function () {
   document.getElementById("root").innerHTML = "";
 
   const root = document.getElementById('root')
-        root.classList = 'HpDataS-style'
+  root.classList = 'HpDataS-style'
 
   const funFact = alldata.funFacts
 
-  const generatorHTML = (funFacts) => {
-    const div = document.createElement('div')
-    const typeFunFact = document.createElement('h2')
-    typeFunFact.textContent = funFacts.type
-    const contentFunFact = document.createElement('h3')
-    contentFunFact.textContent = funFacts.content
-
-    div.append(typeFunFact, contentFunFact)
-
-    return div
-  }
-
-  funFact.forEach(onefunFact=> root.appendChild(generatorHTML(onefunFact)))
+  funFact.forEach(onefunFact => root.appendChild(generatorHTMLff(onefunFact)))
 
 }, true);
 
-
-
-/* -----------------Potions grid------------------*/
+// -------filtrado por objeto (Potions)-----------------
 document.getElementById("potions").addEventListener("click", function () {
   document.getElementById("root").innerHTML = "";
-  
+
   const root = document.getElementById('root')
-        root.classList = 'HpDataS-style'
-    
+  root.classList = 'HpDataS-style'
+
   const potion = alldata.potions
-  
-  const generatorHTML = (potions) => {
-    const div = document.createElement('div')
-    const namePotions = document.createElement('h2')
-    namePotions.textContent = potions.name
-    const descriptionPotions = document.createElement('h3')
-     descriptionPotions.textContent = potions.description
-  
-    div.append(namePotions, descriptionPotions)
-    
-    return div
-    }
-    
-    potion.forEach(onepotions=> root.appendChild(generatorHTML(onepotions)))
-  
-  }, true);
-  
 
+  potion.forEach(onepotions => root.appendChild(generatorHTMLdescribe(onepotions)))
 
-/*------------------ books grid---------------*/
+}, true);
+
+//---------filtrado pot objeto (books)---------------
 document.getElementById("books").addEventListener("click", function () {
   document.getElementById("root").innerHTML = "";
 
   const root = document.getElementById('root')
-        root.classList = 'HpDataS-style'
- 
+  root.classList = 'HpDataS-style'
+
   const book = alldata.books
 
-  const generatorHTML = (books) => {
-  const div = document.createElement('div')
-  const titleBook = document.createElement('h2')
-  titleBook.textContent = books.title
-  const descriptionBook = document.createElement('h3')
-  descriptionBook.textContent = books.description
+  book.forEach(onebook => root.appendChild(generatorHTMLbook(onebook)))
 
-  div.append(titleBook, descriptionBook)
+}, true);
 
-  return div
+//---------uso de función Filter para clasificación por clase----------
+
+document.getElementById("seleccion").addEventListener("click", function () {
+  document.getElementById("root").innerHTML = "";
+  console.log(seleccion.value);
+
+
+  const root = document.getElementById('root')
+  root.classList = 'HpDataF-style'
+
+  const character = alldata.characters
+
+
+  const wordfilter = seleccion.value
+  console.log(seleccion.value)
+  if (wordfilter == "Gryffindor" || wordfilter == "Ravenclaw" || wordfilter == "Slytherin" || wordfilter == "Hufflepuff") {
+    filterbyword(alldata, wordfilter).forEach(onecharacters => root.appendChild(generatorHTMLfilter(onecharacters)))
+  } else {
+    filterbyword2(alldata, wordfilter).forEach(onecharacters => root.appendChild(generatorHTMLfilter(onecharacters)))
   }
 
-   book.forEach(onebook => root.appendChild(generatorHTML(onebook)))
-
-}, true);
+}, true)
 
 
-/*----------Filter functions-------------*/
-/*-------------------------gryffindor------------*/
-document.getElementById("select").addEventListener("click", function () {
-  document.getElementById("root").innerHTML = "";
-  console.log(seleccion.value);
-  
-
-  const root = document.getElementById('root')
-        root.classList = 'HpData-style'
-  
-  const character = alldata.characters
-   
-
-    filterbygrifindor(alldata).forEach(onecharacters => root.appendChild(generatorHTML(onecharacters)))
-
-}, true);
-
-//-----------------------slyfherin
-
-document.getElementById("sl").addEventListener("click", function () {
-  document.getElementById("root").innerHTML = "";
-  console.log(seleccion.value);
-  
-
-  const root = document.getElementById('root')
-        root.classList = 'HpData-style'
-  
-  const character = alldata.characters
-
-  filterbyslytherin(alldata).forEach(onecharacters => root.appendChild(generatorHTML(onecharacters)))
-
-}, true);
- 
